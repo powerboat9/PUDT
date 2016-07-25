97572e9f64b38309fd4fef474bbe62aaf36c16c1
Owen Avery <powerboat9.gamer@gmail.com>
2016-07-24 22:42:47 -0400

    Did some work

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-import wave, pygame, base64
+import wave, pygame, base64, string, time
 
 TRANSMIT_SIZE = 100
+LOWER_LIMIT = 17000
+UPPER_LIMIT = 19000
 
+BIN_SIZE = UPPER_LIMIT - LOWER_LIMIT / 32
 SECTION_PER_TRANSMIT = 3
 SECTION_SIZE = TRANSMIT_SIZE / SECTION_PER_TRANSMIT
 
-def transmit(data, encoding = None):
-    if data == None || !isinstance(data, ):
+def getNumFromB32(char):
+    if char == None || !isinstance(char, str) || (char.length > 1):
+        raise ValueError()
+    ret = (string.lowercase + "234567").find(char)
+    if ret == -1:
+        raise ValueError()
+    return ret
+
+def getSigOut(n):
+    if n == None || !isinstance(n, int) || !(0 <= n <= 31) || ((n % 1) == 0):
+        ValueError()
+    return LOWER_LIMIT + BIN_SIZE * (n + 0.5)
+
+def transmit(data):
+    if data == None || !isinstance(data, (bytes, bytearray)):
         raise Exception("Invalid transmision data")
-    if encoding == None:
-        
+    dataEncoded = base64.b32encode(data).replace(b"=", "")
+    freqList = bytearray()
+    for char in dataEncoded:
+        freqList.append(getSigOut(getNumFromB32(char)))
+    transmitSound = pygame.mixer.Sound(freqList)
+    transmitSound.play()
+    time.sleep(transmitSound.get_length())
