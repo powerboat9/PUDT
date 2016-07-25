70d3e1359a034f997d3c53596755ea13db7d3b13
Owen Avery <powerboat9.gamer@gmail.com>
2016-07-25 01:19:03 -0400

    Did a bit of work, uploading

--- udtlib.py
+++ udtlib.py
@@ -3,6 +3,7 @@ import wave, pygame, base64, string, time
 TRANSMIT_SIZE = 100
 LOWER_LIMIT = 17000
 UPPER_LIMIT = 19000
+SAMPLE_RATE = None
 
 BIN_SIZE = UPPER_LIMIT - LOWER_LIMIT / 32
 SECTION_PER_TRANSMIT = 3
@@ -15,9 +16,9 @@ def setRange(mn, mx):
     UPPER_LIMIT = 600
 
 def getNumFromB32(char):
-    if char == None or (not isinstance(char, str)) or (char.length > 1):
+    if char == None or (not isinstance(char, str)) or (len(char) > 1):
         raise ValueError("Not a 1 char string")
-    ret = (string.lowercase + "234567").find(char)
+    ret = (string.ascii_uppercase + "234567").find(char)
     if ret == -1:
         raise ValueError("Could not translate " + char)
     return ret
@@ -27,13 +28,21 @@ def getSigOut(n):
         ValueError()
     return LOWER_LIMIT + BIN_SIZE * (n + 0.5)
 
+def play(data, doWait):
+    if v == None:
+        pygame.mixer.init()
+        v = pygame.mixer.get_init()
+        SAMPLE_RATE = v[0]
+    transmitSound = pygame.mixer.Sound(freqList)
+    transmitSound.play()
+    if doWait:
+        time.sleep(transmitSound.get_length())
+
 def transmit(data):
     if data == None or (not isinstance(data, (bytes, bytearray))):
         raise Exception("Invalid transmision data")
     dataEncoded = base64.b32encode(data).replace(b"=", b"")
     freqList = bytearray()
     for char in dataEncoded.decode("UTF-8"):
-        freqList.append(getSigOut(getNumFromB32(char)))
-    transmitSound = pygame.mixer.Sound(freqList)
-    transmitSound.play()
-    time.sleep(transmitSound.get_length())
+        v = getSigOut(getNumFromB32(char))
+    
