1e2a59148f417e428943e6d9762e351adf0b914d
powerboat9 <powerboat9@users.noreply.github.com>
2016-12-27 11:31:32 -0500

    Update recv.py

--- recv.py
+++ recv.py
@@ -1,16 +1,23 @@
+import random
+
 EXTENDED_ASCII = 0
 ASCII = 1
 BASE64 = 2
 BASE32 = 3
+BASE16 = 4
+BASE8 = 5
 
 class Portal(object):
     def __init__(mode = 3, waitCallback = None, secs = 0.2):
-        self.binsize = ([256, 128, 64, 32])[mode]
-        if not self.binsize:
+        self.bins = ([256, 128, 64, 32, 16, 8])[mode]
+        if not self.bins:
             raise AttributeException("Invalid mode")
         elif waitCallback != None and not callable(waitCallback):
             raise AttributeException("Invalid callback")
-        elif type(0.2) != 
+        elif type(secs) != "<class 'int'>":
+            raise AttributeException("Invalid frequency duration")
         self.waitCallback = waitCallback
-    def transmit(s):
-        
+        self.secs = secs
+    def raw_transmit(transmitList, minfreq, maxfreq):
+        binsize = (maxfreq - minfreq) / self.bins
+        for freq in transmitList
