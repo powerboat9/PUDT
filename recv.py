22db5c9db1dcfb92b8016dae311dc37972ea3bc0
powerboat9 <powerboat9@users.noreply.github.com>
2016-12-27 11:05:55 -0500

    Update recv.py

--- recv.py
+++ recv.py
@@ -4,12 +4,13 @@ BASE64 = 2
 BASE32 = 3
 
 class Portal(object):
-    def __init__(mode = 3, waitCallback = None):
+    def __init__(mode = 3, waitCallback = None, secs = 0.2):
         self.binsize = ([256, 128, 64, 32])[mode]
         if not self.binsize:
             raise AttributeException("Invalid mode")
         elif waitCallback != None and not callable(waitCallback):
             raise AttributeException("Invalid callback")
+        elif type(0.2) != 
         self.waitCallback = waitCallback
     def transmit(s):
         
